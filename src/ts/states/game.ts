64c0f2c2835b63b4da1abda89b259c6e0c160c94
TheWalruzz <naerian@gmail.com>
2016-12-23 00:09:04 +0100

    Fixed interval issues

--- src/ts/states/game.ts
+++ src/ts/states/game.ts
@@ -1,7 +1,7 @@
 namespace App.State {
     export class Game extends Phaser.State {
         private intervalDuration: number = 1500;
-        private intervalId: number;
+        private interval: Phaser.TimerEvent;
         private points: number = 0;
         private enemies: any = [
             App.Models.Beetle,
@@ -13,22 +13,22 @@ namespace App.State {
 
         public create(): void {
             this.game.world.setBounds(0, 0, this.game.width, this.game.height);
-            this.intervalId = setInterval(this.createNewEnemy.bind(this), this.intervalDuration);
-            this.text = this.game.add.text(this.game.world.centerX, 40, 'Punkty: 0', {
+            this.game.time.events.loop(this.intervalDuration, this.createNewEnemy, this);
+            this.text = this.game.add.text(this.game.width - 15, 40, 'Punkty: 0', {
                 font: 'Arial Black',
                 fontSize: 20,
                 fill: '#003B8A'
             });
-            this.text.anchor.set(0.5);
+            this.text.anchor.set(1, 0.5);
             this.text.align = 'center';
 
-            this.menuButton = this.game.add.sprite(5, 5, 'menu');
+            this.menuButton = this.game.add.sprite(15, 8, 'pause');
             this.menuButton.inputEnabled = true;
             this.menuButton.events.onInputDown.add(() => {
-                window.clearInterval(this.intervalId);
+                this.game.time.events.remove(this.interval);
                 this.game.state.start('Menu');
             }, this);
-            this.menuButton.scale.set(0.2);
+            this.menuButton.scale.set(0.5);
         }
 
         public update(): void {}
--- src/ts/states/preload.ts
+++ src/ts/states/preload.ts
@@ -2,7 +2,8 @@ namespace App.State {
   export class Preload extends Phaser.State {
     public preload(): void {
       this.game.load.image('stain', 'assets/stain.png');
-      this.game.load.image('menu', 'assets/play.png');
+      this.game.load.image('play', 'assets/play.png');
+      this.game.load.image('pause', 'assets/pause.png');
       this.game.load.spritesheet('beetle', 'assets/beetle_base_26x33.png', 26, 33);
       this.game.load.spritesheet('fly', 'assets/fly_32x32.png', 32, 32);
     }
